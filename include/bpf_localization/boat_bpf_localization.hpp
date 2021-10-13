2ce56ff7c2877a396aa229d2d80d18a0dd5ca795
Alexis Mifsud <alexis.mifsud@ensta-paris.fr>
2021-10-13 19:03:51 +0200

    boar can be construct with various initializations

--- include/bpf_localization/boat_bpf_localization.hpp
+++ include/bpf_localization/boat_bpf_localization.hpp
@@ -21,6 +21,46 @@ class BoatBPFLocalization : public BoxParticleFilter
             dynamical_model_ = std::shared_ptr<DynamicalModel>(new IMUDynamicalModel(dt));
         }
 
+        BoatBPFLocalization(double pos, double vel, double theta,
+                            unsigned int N = 20,
+                            double dt = 1.)
+            :BoxParticleFilter(N, get_init(pos, vel, theta)),
+             imu_measures_(IMUDynamicalModel::control_size), 
+             gps_measures_(IMUDynamicalModel::measures_size)
+        {
+            dynamical_model_ = std::shared_ptr<DynamicalModel>(new IMUDynamicalModel(dt));
+        }
+
+        BoatBPFLocalization(Particles& particles,
+                            unsigned int N = 20,
+                            double dt = 1.)
+            :BoxParticleFilter(N, particles),
+             imu_measures_(IMUDynamicalModel::control_size), 
+             gps_measures_(IMUDynamicalModel::measures_size)
+        {
+            dynamical_model_ = std::shared_ptr<DynamicalModel>(new IMUDynamicalModel(dt));
+        }
+
+        const IntervalVector get_init(double pos, double vel, double theta)
+        {
+            assert(pos > 0. && "initial incertitude on position must be > 0");
+            assert(vel > 0. && "initial incertitude on velocity must be > 0");
+            assert(theta > 0. && "initial incertitude on orientation must be > 0");
+
+            IntervalVector initial_box(IMUDynamicalModel::state_size);
+            initial_box[0] = Interval(cos(theta), 1.);
+            initial_box[1] = Interval(sin(-theta)/sqrt(3), sin(theta)/sqrt(3));
+            initial_box[2] = Interval(sin(-theta)/sqrt(3), sin(theta)/sqrt(3));
+            initial_box[3] = Interval(sin(-theta)/sqrt(3), sin(theta)/sqrt(3));
+            initial_box[4] = Interval(-pos, pos);
+            initial_box[5] = Interval(-pos, pos);
+            initial_box[6] = Interval(-pos, pos);
+            initial_box[7] = Interval(-vel, vel);
+            initial_box[8] = Interval(-vel, vel);
+            initial_box[9] = Interval(-vel, vel);
+            return IntervalVector(initial_box);
+        }
+
         void IMUCallback(IntervalVector& imu)
         {
             imu_measures_ = imu;
