a01ea917cb88b9bc13740cd182f5ec7ad5ba1da7
Alexis Mifsud <alexis.mifsud@ensta-paris.fr>
2021-10-14 15:48:14 +0200

    ibex can be not included

--- include/bpf_localization/utils.hpp
+++ include/bpf_localization/utils.hpp
@@ -1,7 +1,6 @@
 #ifndef UTILS
 #define UTILS
 
-#include "ibex/ibex.h"
 #include <ros/ros.h>
 #include <vector>
 #include <utility>
@@ -14,11 +13,16 @@
 
 #define NaN std::numeric_limits<double>::quiet_NaN()
 
+#ifndef WO_IBEX
+
+#include "ibex/ibex.h"
 using namespace ibex;
 
 inline bool eps_equals( IntervalVector v1, IntervalVector v2, double eps = 1e-13)
 {
     return v1.is_subset(IntervalVector(v2).inflate(eps)) && v2.is_subset(v1.inflate(eps));
 }
+
+#endif
 
 #endif
