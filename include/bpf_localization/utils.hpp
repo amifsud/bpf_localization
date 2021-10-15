25b698de6bddf30bfc63cbb94c4157d6cc9272ce
Alexis Mifsud <alexis.mifsud@ensta-paris.fr>
2021-10-15 18:37:34 +0200

    add IntervalVectorXYZ

--- include/bpf_localization/utils.hpp
+++ include/bpf_localization/utils.hpp
@@ -23,6 +23,32 @@ inline bool eps_equals( IntervalVector v1, IntervalVector v2, double eps = 1e-13
     return v1.is_subset(IntervalVector(v2).inflate(eps)) && v2.is_subset(v1.inflate(eps));
 }
 
+class IntervalVectorXYZ
+{
+    public:
+        Interval x;
+        Interval y;
+        Interval z;
+
+    public:
+        IntervalVectorXYZ()
+        {
+            setZero();
+        }
+
+        void setZero()
+        {
+            x = Interval(0.,0.);
+            y = Interval(0.,0.);
+            z = Interval(0.,0.);
+        }
+};
+
+ostream& operator<<(ostream& os, const IntervalVectorXYZ& i)
+{
+    os << i.x << i.y << i.z;
+    return os;
+}
 #endif
 
 #endif
