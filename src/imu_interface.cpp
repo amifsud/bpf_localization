e520e6f48e9cf5fa187f158ae3a778ea4522cdd2
Alexis Mifsud <alexis.mifsud@ensta-paris.fr>
2021-10-15 18:37:20 +0200

    update imu calibration

--- src/imu_interface.cpp
+++ src/imu_interface.cpp
@@ -1,54 +1,105 @@
-#define WO_IBEX
 #include "bpf_localization/utils.hpp"
 
 #include <message_filters/subscriber.h>
 #include <sensor_msgs/Imu.h>
 #include "bpf_localization/Calibration.h"
 
-class IMU
+class IMUInterface
 {
-    public:
-        ros::Publisher  imu_pub_;
+    protected:
+        // ROS IMU
         ros::Subscriber imu_sub_;
+        
+        // Calibration
         ros::ServiceServer calibration_server_;
+        ros::Time until_;
+        std::vector<Vector> data_;
+        Vector mean_;
+        Vector tmp_;
+        double nb_;
+        bool calibration_;
 
-        sensor_msgs::Imu imu_msg_;
+        // Interval IMU
+        std::string name_;
+        Vector diameters_;
 
     public:
-        IMU(ros::NodeHandle* nh)
+        IMUInterface(ros::NodeHandle* nh, std::string name):
+            calibration_(false), name_(name), diameters_(6, 0.),
+            mean_(Vector(6,0.)), nb_(0), tmp_(6)
         {
-            imu_pub_ = nh->advertise<sensor_msgs::Imu>("imu_out", 50);
-            imu_sub_ = nh->subscribe("imu_in", 50, &IMU::callback, this);
+            imu_sub_ = nh->subscribe("imu_in", 50, &IMUInterface::callback, this);
+            
             calibration_server_ 
-                = nh->advertiseService("imu_calibration", &IMU::calibration, this);
+                = nh->advertiseService("imu_calibration", &IMUInterface::calibration, this);
         }
 
+    protected:
         bool calibration(bpf_localization::Calibration::Request  &req,
                          bpf_localization::Calibration::Response &res)
         {
-            res.angular_velocity_diameters.x = 2.;
-            res.angular_velocity_diameters.y = 2.;
-            res.angular_velocity_diameters.z = 2.;
-            res.linear_acceleration_diameters.x = 2.;
-            res.linear_acceleration_diameters.y = 2.;
-            res.linear_acceleration_diameters.z = 2.;
-
+            calibration_ = true;
+            mean_ = Vector(6, 0.);
+            nb_ = 0.;
+            data_.clear();
+            until_ = ros::Time::now() + ros::Duration(req.calibration_duration, 0);
             return true;
         }
 
         void callback(const sensor_msgs::Imu& imu_in) 
         {
-            imu_msg_ = imu_in;
-            imu_msg_.header.frame_id = "imu_calibrated";
-            
-            imu_msg_.angular_velocity.x    = imu_in.angular_velocity.x;
-            imu_msg_.angular_velocity.y    = imu_in.angular_velocity.y;
-            imu_msg_.angular_velocity.z    = imu_in.angular_velocity.z;
-            imu_msg_.linear_acceleration.x = imu_in.linear_acceleration.x;
-            imu_msg_.linear_acceleration.y = imu_in.linear_acceleration.y;
-            imu_msg_.linear_acceleration.z = imu_in.linear_acceleration.z;
+            if(!calibration_)
+            {
+                ROS_INFO_STREAM(diameters_);
+            }
+            else
+            {
+                ROS_INFO_STREAM("Calibrating...");
+                feed_calibration(imu_in);
+            }
+        }
 
-            imu_pub_.publish(imu_msg_);
+        Vector computeInterval()
+        {
+            Vector diameters(6, 0.);
+
+            double number;
+            for(auto vect = data_.begin(); vect !=data_.end(); vect++)
+            {
+                for(unsigned int i = 0; i < 6; ++i)
+                {
+                    number = 4*std::abs(vect->operator[](i)-mean_[i]/nb_);
+                    if(number > diameters[i]) diameters[i] = number;
+                }
+            }
+
+            ROS_INFO_STREAM(diameters);
+
+            return diameters;
+        }
+
+        void feed_calibration(const sensor_msgs::Imu& imu_in)
+        {
+            if(ros::Time::now().toSec() < until_.toSec())
+            {
+                tmp_[0] = imu_in.angular_velocity.x;
+                tmp_[1] = imu_in.angular_velocity.y;
+                tmp_[2] = imu_in.angular_velocity.z;
+                tmp_[3] = imu_in.linear_acceleration.x;
+                tmp_[4] = imu_in.linear_acceleration.y;
+                tmp_[5] = imu_in.linear_acceleration.z;
+
+                mean_ += tmp_;
+                data_.push_back(Vector(tmp_));
+                nb_ += 1.;
+
+                diameters_ = computeInterval(); 
+            }
+            else
+            {
+                calibration_ = false;
+                diameters_ = computeInterval(); 
+            }
         }
 };
 
@@ -57,7 +108,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "imu_interface");
     ros::NodeHandle nh;
 
-    IMU imu = IMU(&nh);
+    auto imu = IMUInterface(&nh, "boat_imu");
 
     ros::Rate r(5.0);
     while(nh.ok())
