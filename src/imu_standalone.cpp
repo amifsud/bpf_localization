7c7044a0d65b2ddfaf61608201c5d656b405463b
Alexis Mifsud <alexis.mifsud@ensta-paris.fr>
2021-10-20 20:07:19 +0200

    update imu_calibration

--- src/imu_standalone.cpp
+++ src/imu_standalone.cpp
@@ -9,28 +9,11 @@ int main(int argc, char** argv)
 
     auto imu = IMUInterface(&nh, "boat_imu", 3);
 
-    ros::Rate r(1./5);
+    ros::Rate r(5.);
 
-    std::deque<IntervalVector> data;
-
-    r.sleep();
-
-    data = imu.getIntervalData();
-    ROS_INFO_STREAM("SIZE = " << data.size());
-    
-    ros::spinOnce();
-
-    r.sleep();
-
-    data = imu.getIntervalData();
-    ROS_INFO_STREAM("SIZE = " << data.size());
-    for(auto it = data.begin(); it != data.end(); it++)
-        ROS_INFO_STREAM(*it);
-    ros::spinOnce();
-
-    /*while(nh.ok())
+    while(nh.ok())
     {
         ros::spinOnce();
         r.sleep();
-    }*/
+    }
 }
