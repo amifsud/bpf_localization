fde6d08a66fe7447b9f1e7f3445f49f70b71f35f
Alexis Mifsud <alexis.mifsud@ensta-paris.fr>
2021-10-22 19:22:34 +0200

    add density distribution method

--- python/bpf_localization/plot_imu_calibration.py
+++ python/bpf_localization/plot_imu_calibration.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python
 from matplotlib import pyplot as plt
 import numpy as np
+from enum import Enum
+
+class Methods(Enum):
+    TIME_DEPENDENT = 1
+    DENSITY_DISTRIBUTION = 2
 
 class CallbackTriggeredPlotting(object):
-    def __init__(self, i, j):
+    def __init__(self, i, j, method):
         plt.ion()
         self.fig, self.ax = plt.subplots(i, j, figsize=(10, 8))
         self.data_size = 0
         self.vectors = None
+        self.method = method
 
     def __del__(self):
         plt.close(self.fig)
 
-    def plotVector(self, vector, nb):
-        for i in range(len(vector)):
-            self.ax[nb, i].set_xlabel(self.labels[nb][i])
-            self.ax[nb, i].set_ylim([min(vector[i]), max(vector[i])])
-            line, = self.ax[nb, i].plot(vector[i], 'x', color='blue')
-            line.set_ydata(vector[i])
+    def timeDependent(self, vector, i, u):
+        self.ax[i, u].set_xlabel(self.labels[i][u])
+        self.ax[i, u].set_ylim([min(vector), max(vector)])
+        line, = self.ax[i, u].plot(vector, 'x', color='blue')
+
+    def densityDistribution(self, vector, i, u):
+        self.ax[i, u].set_xlabel(self.labels[i][u])
+        lb = min(vector)
+        ub = max(vector)
+        self.ax[i, u].set_xlim([lb, ub])
+        bins = 20 
+        line = self.ax[i, u].hist(vector, bins)
+
+    def plotMethod(self, vector, i, u):
+        if self.method == Methods.TIME_DEPENDENT : 
+            self.timeDependent(vector, i, u)
+        elif self.method == Methods.DENSITY_DISTRIBUTION : 
+            self.densityDistribution(vector, i, u)
+        else : raise("Bad plot method") 
 
     def plot(self):
         if self.vectors == None: raise("vectors has to be initialized")
         if len(self.vectors[0][0]) > self.data_size:
             for i in range(len(self.vectors)):
-                self.plotVector(self.vectors[i], i)
+                for u in range(len(self.vectors[i])):
+                    self.plotMethod(self.vectors[i][u], i, u)
             self.fig.canvas.flush_events()
             self.fig.canvas.draw()
             self.data_size = len(self.vectors[0][0])
@@ -32,8 +52,8 @@ class CallbackTriggeredPlotting(object):
         raise("Feed method has to be implemented")
 
 class PlotImuCalibration(CallbackTriggeredPlotting):
-    def __init__(self):
-        CallbackTriggeredPlotting.__init__(self, 2, 3)
+    def __init__(self, method):
+        CallbackTriggeredPlotting.__init__(self, 2, 3, method)
         self.labels = [["angular_velocity x",
                         "angular_velocity y",
                         "angular_velocity z",],
--- scripts/plot_imu_calibration_node.py
+++ scripts/plot_imu_calibration_node.py
@@ -8,8 +8,8 @@ from sensor_msgs.msg import Imu
 from bpf_localization.plot_imu_calibration import *
 
 class PlotImuCalibrationROS(PlotImuCalibration):
-    def __init__(self):
-        PlotImuCalibration.__init__(self)
+    def __init__(self, method):
+        PlotImuCalibration.__init__(self, method)
         rospy.Subscriber("/imu", Imu, self.callback)
 
     def callback(self, data):
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     rospy.init_node('py_plot_imu', anonymous = True)
 
     duration = 10.
-    plot_imu = PlotImuCalibrationROS()
+    plot_imu = PlotImuCalibrationROS(Methods.DENSITY_DISTRIBUTION)
 
     while 1: plot_imu.plot()
 
